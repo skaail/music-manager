6148cb028ae9615700594508fa9e1d4b95f0f114
Bruno <bruno.gianini@readmore.com.br>
2023-01-30 02:08:02 -0300

    Formulário inserindo artistas

--- src/pages/CadastroArtista.js
+++ src/pages/CadastroArtista.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from "react"
 
 function Cadastro() {
+  const [name, setName] = useState("");
+
+  const onSubmitHandler = (e) =>{
+    e.preventDefault()
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: name })
+    }
+    
+
+    fetch('http://localhost:3000/artist', requestOptions)
+          .then(response => response.json())
+    
+  }
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
-    <div>Cadastro</div>
+    <Form onSubmit={onSubmitHandler}>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Nome</Form.Label>
+      <Form.Control type="text" placeholder="Digite o nome do artista" onChange={handleChange}/>
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
   )
 }
 
