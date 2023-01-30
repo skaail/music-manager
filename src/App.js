77d74a43c3416f20eebdb325ea4c906b79c36959
Bruno <bruno.gianini@readmore.com.br>
2023-01-29 22:52:44 -0300

    Nav Dropdown

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
-import Cadastro from './pages/Cadastro';
+import CadastroArtista from './pages/CadastroArtista';
 
 function App() {
   return (
@@ -16,15 +17,27 @@ function App() {
           <Navbar.Brand href="#home">SpotiMan</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="cadastro">Cadastro</Nav.Link>
+
+            <NavDropdown title="Cadastro" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/cadastro-artista">Artista</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Albúm
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Playlist
+              </NavDropdown.Item>
+            </NavDropdown>
+
             <Nav.Link href="#pricing">Relatório</Nav.Link>
+
           </Nav>
         </Container>
       </Navbar>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path='/cadastro' element={<Cadastro />} />
+          <Route path='/cadastro-artista' element={<CadastroArtista />} />
         </Routes>
       </BrowserRouter>
     </>
