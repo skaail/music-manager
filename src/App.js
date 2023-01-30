dcb9543c2ec1c278b0ef8c1cbaf5a57dfc21780e
Bruno <bruno.gianini@readmore.com.br>
2023-01-29 22:49:32 -0300

    Routes Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">SpotiMan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="cadastro">Cadastro</Nav.Link>
+            <Nav.Link href="#pricing">Relatório</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/cadastro' element={<Cadastro />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
